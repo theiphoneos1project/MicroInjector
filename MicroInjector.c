547494bd8b478a04ca473e3b0bbd8b2e50ae6b74
NightwindDev <81449663+NightwindDev@users.noreply.github.com>
2026-02-22 15:31:35 -0500

    Add HookMessage (with warning)

--- MicroInjector.c
+++ MicroInjector.c
@@ -1,5 +1,6 @@
 #include "MicroInjector.h"
 #include <objc/runtime.h>
+#include <string.h>
 
 void HookMessageEx(const Class klass, const SEL selector, IMP implementation, IMP *original) {
     if (klass == NULL || selector == NULL || implementation == NULL) {
@@ -23,4 +24,37 @@ void HookMessageEx(const Class klass, const SEL selector, IMP implementation, IM
             *original = method_setImplementation(method, implementation);
         }
     }
+}
+
+IMP HookMessage(const Class klass, const SEL selector, IMP implementation, const char *prefix) {
+    if (klass == NULL || selector == NULL || implementation == NULL) {
+        return NULL;
+    }
+
+    Method const method = class_getInstanceMethod(klass, selector);
+    if (method == NULL) {
+        return NULL;
+    }
+
+    if (prefix != NULL) {
+        const char *const name = sel_getName(selector);
+        const char *const typeEncoding = method_getTypeEncoding(method);
+        
+        IMP const original = method_getImplementation(method);
+
+        const size_t prefixLength = strlen(prefix);
+        const size_t nameLength = strlen(name);
+
+        char renamedName[prefixLength + nameLength + 1];
+        bcopy(prefix, renamedName, prefixLength);
+        bcopy(name, renamedName + prefixLength, nameLength + 1);
+
+        class_addMethod(klass, sel_registerName(renamedName), original, typeEncoding);
+        method_setImplementation(method, implementation);
+        return NULL;
+    }
+
+    IMP original = NULL;
+    HookMessageEx(klass, selector, implementation, &original);
+    return original;
 }
--- MicroInjector.h
+++ MicroInjector.h
@@ -7,8 +7,7 @@
 extern "C" {
 #endif // __cplusplus
 
-#if defined(clang)
-#if __has_feature(nullability)
+#if defined(__clang__) && __has_feature(nullability)
 #define MI_NONNULL _Nonnull
 #define MI_NULLABLE _Nullable
 #define MI_NULL_UNSPECIFIED _Null_unspecified
@@ -16,14 +15,15 @@ extern "C" {
 #define MI_NONNULL
 #define MI_NULLABLE
 #define MI_NULL_UNSPECIFIED
-#endif // __has_feature(nullability)
-#else
-#define MI_NONNULL
-#define MI_NULLABLE
-#define MI_NULL_UNSPECIFIED
-#endif // defined(__has_feature)
+#endif // defined(__clang__) && __has_feature(nullability)
 
 void HookMessageEx(MI_NONNULL const Class klass, MI_NONNULL const SEL selector, MI_NONNULL IMP implementation, MI_NULLABLE IMP *MI_NULLABLE original);
+
+__deprecated_msg(
+    "HookMessage is deprecated and is only implementated for backwards-compatibility. "
+    "DEVELOPERS SHOULD NOT USE THIS API. More info: https://www.cydiasubstrate.com/api/c/MSHookMessage/"
+)
+MI_NULLABLE IMP HookMessage(MI_NONNULL const Class klass, MI_NONNULL SEL selector, MI_NONNULL IMP implementation, const char *MI_NULLABLE prefix); 
 
 #ifdef __cplusplus
 }
