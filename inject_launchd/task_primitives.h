8ce727136e26fd1e819523bf612d53bb6d27f044
NightwindDev <81449663+NightwindDev@users.noreply.github.com>
2026-07-14 15:31:41 -0400

    chore: fix header guard name

--- inject_launchd/task_primitives.h
+++ inject_launchd/task_primitives.h
@@ -2,8 +2,8 @@
 // Thanks to staturnz for the huge amount of help!
 //
 
-#ifndef TASK_PRIMITIVES
-#define TASK_PRIMITIVES
+#ifndef TASK_PRIMITIVES_H
+#define TASK_PRIMITIVES_H
 
 #include <mach/mach.h>
 
@@ -12,4 +12,4 @@ kern_return_t task_deallocate(mach_port_t task, uint32_t address, size_t size);
 kern_return_t task_protect(mach_port_t task, uint32_t address, uint32_t size, vm_prot_t protectionFlags);
 kern_return_t task_write(mach_port_t task, uint32_t address, void *buffer, uint32_t size); 
 
-#endif // TASK_PRIMITIVES
+#endif // TASK_PRIMITIVES_H
